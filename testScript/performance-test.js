fd2e69d1556467881ccf1566cc702324ad2c9de2
Vishesh Rawal <92795514+visheshrwl@users.noreply.github.com>
2023-10-08 12:35:33 +0000

    changes in test

--- testScript/performance-test.js
+++ testScript/performance-test.js
@@ -11,21 +11,25 @@ const browsers = ['chromium', 'firefox', 'webkit'];
     const browser = await chromium.launch(); // Use browserType (e.g., 'firefox') for Firefox
     const page = await browser.newPage();
 
-    // Start Lighthouse
-    const { lhr } = await lighthouse(websiteUrl, {
-      port: (new URL(browser.wsEndpoint())).port,
-      output: 'html', // You can also use 'json' for JSON output
-    });
+    try {
+      // Start Lighthouse
+      const { lhr } = await lighthouse(websiteUrl, {
+        port: (new URL(browser.wsEndpoint())).port,
+        output: 'html', // You can also use 'json' for JSON output
+      });
 
-    // Output Lighthouse results
-    console.log(`Lighthouse Report for ${browserType}:`);
-    console.log(`Performance Score: ${lhr.categories.performance.score * 100}`);
-    console.log(`First Contentful Paint: ${lhr.audits['first-contentful-paint'].displayValue}`);
-    console.log(`Largest Contentful Paint: ${lhr.audits['largest-contentful-paint'].displayValue}`);
-    console.log(`Total Blocking Time: ${lhr.audits['total-blocking-time'].displayValue}`);
-    console.log(`Speed Index: ${lhr.audits['speed-index'].displayValue}`);
-    console.log(`Time to Interactive: ${lhr.audits['interactive'].displayValue}`);
-
-    await browser.close();
+      // Output Lighthouse results
+      console.log(`Lighthouse Report for ${browserType}:`);
+      console.log(`Performance Score: ${lhr.categories.performance.score * 100}`);
+      console.log(`First Contentful Paint: ${lhr.audits['first-contentful-paint'].displayValue}`);
+      console.log(`Largest Contentful Paint: ${lhr.audits['largest-contentful-paint'].displayValue}`);
+      console.log(`Total Blocking Time: ${lhr.audits['total-blocking-time'].displayValue}`);
+      console.log(`Speed Index: ${lhr.audits['speed-index'].displayValue}`);
+      console.log(`Time to Interactive: ${lhr.audits['interactive'].displayValue}`);
+    } catch (error) {
+      console.error(`Error running Lighthouse: ${error.message}`);
+    } finally {
+      await browser.close();
+    }
   }
 })();
