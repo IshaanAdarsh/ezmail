ecda78affef04ff5fe2753599fcefc407f4ac08b
Vishesh Rawal <92795514+visheshrwl@users.noreply.github.com>
2023-10-08 12:48:04 +0000

    Used puppeteer

--- testScript/performance-test.js
+++ testScript/performance-test.js
@@ -1,35 +1,34 @@
-import { chromium } from 'playwright';
-import lighthouse from 'lighthouse';
-import { URL } from 'url';
-
-const websiteUrl = 'ezmail-home.vercel.app/';
-
-const browsers = ['chromium', 'firefox', 'webkit'];
+import puppeteer from 'puppeteer';
 
 (async () => {
-  for (const browserType of browsers) {
-    const browser = await chromium.launch(); // Use browserType (e.g., 'firefox') for Firefox
-    const page = await browser.newPage();
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
 
-    try {
-      // Start Lighthouse
-      const { lhr } = await lighthouse(websiteUrl, {
-        port: (new URL(browser.wsEndpoint())).port,
-        output: 'html', // You can also use 'json' for JSON output
-      });
+  try {
+    // Navigate to a URL and wait for network idle
+    const navigationPromise = page.waitForNavigation({ waitUntil: 'networkidle0' });
+    await page.goto('https://ezmail-home.vercel.app/');
 
-      // Output Lighthouse results
-      console.log(`Lighthouse Report for ${browserType}:`);
-      console.log(`Performance Score: ${lhr.categories.performance.score * 100}`);
-      console.log(`First Contentful Paint: ${lhr.audits['first-contentful-paint'].displayValue}`);
-      console.log(`Largest Contentful Paint: ${lhr.audits['largest-contentful-paint'].displayValue}`);
-      console.log(`Total Blocking Time: ${lhr.audits['total-blocking-time'].displayValue}`);
-      console.log(`Speed Index: ${lhr.audits['speed-index'].displayValue}`);
-      console.log(`Time to Interactive: ${lhr.audits['interactive'].displayValue}`);
-    } catch (error) {
-      console.error(`Error running Lighthouse: ${error.message}`);
-    } finally {
-      await browser.close();
-    }
+    // Ensure navigation completed successfully
+    await navigationPromise;
+
+    // Enable Performance API
+    const client = await page.target().createCDPSession();
+    await client.send('Performance.enable');
+
+    // Collect performance metrics
+    const { result } = await client.send('Performance.getMetrics');
+
+    // Evaluate the window.performance object
+    const lighthouseReport = await page.evaluate(() => window.performance.toJSON());
+
+    console.log('Performance Metrics:');
+    console.log(result);
+    console.log('Lighthouse Report:');
+    console.log(lighthouseReport);
+  } catch (error) {
+    console.error(`Error running performance test: ${error}`);
+  } finally {
+    await browser.close();
   }
 })();
