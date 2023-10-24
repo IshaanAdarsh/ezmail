ec9f3e1f5017e3fc08a7bc17035950df98f69ed0
Bhushan <alberteinstein02.99792458>
2023-10-24 20:11:14 +0530

    fixed server side

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -2,7 +2,7 @@
   import TagCloud from "TagCloud";
   const container = ".content";
   import { onMount } from "svelte";
-  import Carousel from 'svelte-carousel';
+  import Carousel from "svelte-carousel";
   import { Octokit } from "octokit";
 
   const texts = [
@@ -27,9 +27,9 @@
 
   let contributors = [];
 
-  octokit
-    .request("GET /repos/IshaanAdarsh/ezmail/contributors")
-    .then((res) => {contributors = [...res.data]})
+  octokit.request("GET /repos/IshaanAdarsh/ezmail/contributors").then((res) => {
+    contributors = [...res.data];
+  });
 
   onMount(() => {
     TagCloud(container, texts, options);
@@ -37,9 +37,9 @@
 </script>
 
 <svelte:window bind:innerWidth={ss} />
-<br/>
+<br />
 <h1
-  class="text-center mb-4 text-6xl font-extrabold tracking-tight leading-none  md:text-5xl lg:text-6xl text-gray-800 dark:text-white"
+  class="text-center mb-4 text-6xl font-extrabold tracking-tight leading-none md:text-5xl lg:text-6xl text-gray-800 dark:text-white"
 >
   About Us
 </h1>
@@ -87,9 +87,9 @@
     </li>
   </ul>
 </div>
-<br/>
+<br />
 <h1
-  class="text-center mb-4 text-6xl font-extrabold tracking-tight leading-none  md:text-5xl lg:text-6xl text-white"
+  class="text-center mb-4 text-6xl font-extrabold tracking-tight leading-none md:text-5xl lg:text-6xl text-white"
 >
   <span
     class="text-transparent bg-clip-text bg-gradient-to-b from-gray-900 via-purple-900 to-violet-600"
@@ -98,45 +98,44 @@
 </h1>
 
 <div class="px-4 md:px-0 text-gray-500 dark:text-gray-400">
-  <p
-    class="mb-2 text-xl font-normal text-center lg:text-xl sm:px-16 xl:px-48"
-  >
+  <p class="mb-2 text-xl font-normal text-center lg:text-xl sm:px-16 xl:px-48">
     We believe that the key to success in web development is staying up-to-date
     with the latest technologies and best practices. Our tech stack is built on
     a combination of cutting-edge technologies that allow us to create fast,
     efficient, and user-friendly web applications.Our core tech stack includes:<br
     /><br />
   </p>
-  <span class="content tagcloud  text-2xl   mx-auto text-purple-600 " />
+  <span class="content tagcloud text-2xl mx-auto text-purple-600" />
 </div>
 
 <br /><br /><br />
 
 {#if contributors.length}
   <h1
-  class="my-2 text-center text-4xl font-extrabold tracking-tight leading-none  md:text-4xl lg:text-4xl text-white"
+    class="my-2 text-center text-4xl font-extrabold tracking-tight leading-none md:text-4xl lg:text-4xl text-white"
   >
     <span
-    id="problems" class="mt-6 text-transparent bg-clip-text bg-[radial-gradient(ellipse_at_top_left,_var(--tw-gradient-stops))] from-gray-900 via-purple-900 to-violet-600"
+      id="problems"
+      class="mt-6 text-transparent bg-clip-text bg-[radial-gradient(ellipse_at_top_left,_var(--tw-gradient-stops))] from-gray-900 via-purple-900 to-violet-600"
     >
       Contributors
     </span>
   </h1>
 
   {#if ss < 768}
-    <Carousel
-    autoplayDuration={2500}
-    >
+    <Carousel autoplayDuration={2500}>
       {#each contributors as contributor}
-        <div
-        class="flex items-center justify-center my-8"
-        >
+        <div class="flex items-center justify-center my-8">
           <a
-          href={contributor.html_url} 
-          target="_blank"
-          class="text-[#00acee]  hover:text-black dark:hover:text-white flex flex-col items-center justify-center"
+            href={contributor.html_url}
+            target="_blank"
+            class="text-[#00acee] hover:text-black dark:hover:text-white flex flex-col items-center justify-center"
           >
-            <img src={contributor.avatar_url} class="rounded-full h-40 w-40" alt="avatar" />
+            <img
+              src={contributor.avatar_url}
+              class="rounded-full h-40 w-40"
+              alt="avatar"
+            />
             <h3>{contributor.login}</h3>
           </a>
         </div>
@@ -144,20 +143,19 @@
     </Carousel>
   {:else if ss < 1024}
     <div class="mx-16">
-      <Carousel
-      particlesToShow={2}
-      autoplayDuration={2500}
-      >
+      <Carousel particlesToShow={2} autoplayDuration={2500}>
         {#each contributors as contributor}
-          <div
-          class="flex items-center justify-center my-8"
-          >
+          <div class="flex items-center justify-center my-8">
             <a
-            href={contributor.html_url} 
-            target="_blank"
-            class="text-[#00acee]  hover:text-black dark:hover:text-white flex flex-col items-center justify-center"
+              href={contributor.html_url}
+              target="_blank"
+              class="text-[#00acee] hover:text-black dark:hover:text-white flex flex-col items-center justify-center"
             >
-              <img src={contributor.avatar_url} class="rounded-full h-40 w-40" alt="avatar" />
+              <img
+                src={contributor.avatar_url}
+                class="rounded-full h-40 w-40"
+                alt="avatar"
+              />
               <h3>{contributor.login}</h3>
             </a>
           </div>
@@ -167,20 +165,22 @@
   {:else if ss < 1440}
     <div class="mx-20">
       <Carousel
-      particlesToShow={3}
-      particlesToScroll={2}
-      autoplayDuration={2500}
+        particlesToShow={3}
+        particlesToScroll={2}
+        autoplayDuration={2500}
       >
         {#each contributors as contributor}
-          <div
-          class="flex items-center justify-center my-8"
-          >
+          <div class="flex items-center justify-center my-8">
             <a
-            href={contributor.html_url} 
-            target="_blank"
-            class="text-[#00acee]  hover:text-black dark:hover:text-white flex flex-col items-center justify-center"
+              href={contributor.html_url}
+              target="_blank"
+              class="text-[#00acee] hover:text-black dark:hover:text-white flex flex-col items-center justify-center"
             >
-              <img src={contributor.avatar_url} class="rounded-full h-40 w-40" alt="avatar" />
+              <img
+                src={contributor.avatar_url}
+                class="rounded-full h-40 w-40"
+                alt="avatar"
+              />
               <h3>{contributor.login}</h3>
             </a>
           </div>
@@ -190,20 +190,22 @@
   {:else}
     <div class="mx-44">
       <Carousel
-      particlesToShow={4}
-      particlesToScroll={2}
-      autoplayDuration={2500}
+        particlesToShow={4}
+        particlesToScroll={2}
+        autoplayDuration={2500}
       >
         {#each contributors as contributor}
-          <div
-          class="flex items-center justify-center my-8"
-          >
+          <div class="flex items-center justify-center my-8">
             <a
-            href={contributor.html_url} 
-            target="_blank"
-            class="text-[#00acee]  hover:text-black dark:hover:text-white flex flex-col items-center justify-center"
+              href={contributor.html_url}
+              target="blank"
+              class="text-[#00acee] hover:text-black dark:hover:text-white flex flex-col items-center justify-center"
             >
-              <img src={contributor.avatar_url} class="rounded-full h-40 w-40" alt="avatar" />
+              <img
+                src={contributor.avatar_url}
+                class="rounded-full h-40 w-40"
+                alt="avatar"
+              />
               <h3>{contributor.login}</h3>
             </a>
           </div>
@@ -216,10 +218,11 @@
 <br /><br /><br />
 
 <h1
-  class="mt-2 text-center mb-4 text-4xl font-extrabold tracking-tight leading-none  md:text-4xl lg:text-4xl text-white"
+  class="mt-2 text-center mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-4xl lg:text-4xl text-white"
 >
   <span
-    id="problems" class="mt-6 text-transparent bg-clip-text bg-[radial-gradient(ellipse_at_top_left,_var(--tw-gradient-stops))] from-gray-900 via-purple-900 to-violet-600"
+    id="problems"
+    class="mt-6 text-transparent bg-clip-text bg-[radial-gradient(ellipse_at_top_left,_var(--tw-gradient-stops))] from-gray-900 via-purple-900 to-violet-600"
     >Navigating Problems</span
   >
 </h1>
@@ -274,40 +277,70 @@
   </p>
 </div>
 <section class="bg-neutral-100 dark:bg-gray-900">
-    <div class="py-8 px-4 mx-auto max-w-screen-xl text-center lg:py-16 lg:px-6">
-      <div class="mx-auto mb-8 max-w-screen-md lg:mb-16">
-        <h2 class="mb-4 text-4xl tracking-tight font-extrabold text-gray-800 dark:text-white">
-          Contact Us
-        </h2>
-        <p class="font-light  sm:text-xl text-gray-500 dark:text-gray-400">
-          We're committed to making email communication easy!<br>
-          Simplify Your Email Experience - Get in Touch!
-          <br /><br />
-        </p>
+  <div class="py-8 px-4 mx-auto max-w-screen-xl text-center lg:py-16 lg:px-6">
+    <div class="mx-auto mb-8 max-w-screen-md lg:mb-16">
+      <h2
+        class="mb-4 text-4xl tracking-tight font-extrabold text-gray-800 dark:text-white"
+      >
+        Contact Us
+      </h2>
+      <p class="font-light sm:text-xl text-gray-500 dark:text-gray-400">
+        We're committed to making email communication easy!<br />
+        Simplify Your Email Experience - Get in Touch!
+        <br /><br />
+      </p>
 
-          
-            <div class="flex-col">  <!--body -->
-                <form class="flex flex-col gap-4 " onsubmit="sendEmail(); reset(); return false;" >
-                    <input class="rounded-lg py-2 px-5 w-full bg-transparent border text-black dark:text-white" type="text" id="name" placeholder="Your Name" required>
-                    <input class="rounded-lg py-2 px-5 w-full bg-transparent border text-black dark:text-white" type="email" id="email" placeholder="Email id" required>
-                    <input class="rounded-lg py-2 px-5 w-full bg-transparent border text-black dark:text-white" type="text" id="phone" placeholder="Phone No." required>
-                    <textarea class="rounded-lg py-2 px-5 w-full bg-transparent border text-black dark:text-white" id="message" rows="4" placeholder="How can I help you?"></textarea>
-                    <button  class="inline-flex items-center px-7 py-3 bg-primary-700 hover:bg-primary-800 text-white text-lg rounded-lg focus:outline-none hover:cursor-pointer mx-auto mt-5" id="submit">Send <svg
-                      class="w-5 h-5 ml-2 -mr-1"
-                      fill="currentColor"
-                      viewBox="0 0 20 20"
-                      xmlns="http://www.w3.org/2000/svg"
-                      ><path
-                        fill-rule="evenodd"
-                        d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
-                        clip-rule="evenodd"
-                      /></svg
-                    ></button>
-                </form>  
-            </div>
-       
-      </div>
-      <div class="grid gap-16 grid-cols-2">
+      <div class="flex-col">
+        <!--body -->
+        <form
+          class="flex flex-col gap-4"
+          onsubmit="sendEmail(); reset(); return false;"
+        >
+          <input
+            class="rounded-lg py-2 px-5 w-full bg-transparent border text-black dark:text-white"
+            type="text"
+            id="name"
+            placeholder="Your Name"
+            required
+          />
+          <input
+            class="rounded-lg py-2 px-5 w-full bg-transparent border text-black dark:text-white"
+            type="email"
+            id="email"
+            placeholder="Email id"
+            required
+          />
+          <input
+            class="rounded-lg py-2 px-5 w-full bg-transparent border text-black dark:text-white"
+            type="text"
+            id="phone"
+            placeholder="Phone No."
+            required
+          />
+          <textarea
+            class="rounded-lg py-2 px-5 w-full bg-transparent border text-black dark:text-white"
+            id="message"
+            rows="4"
+            placeholder="How can I help you?"
+          />
+          <button
+            class="inline-flex items-center px-7 py-3 bg-primary-700 hover:bg-primary-800 text-white text-lg rounded-lg focus:outline-none hover:cursor-pointer mx-auto mt-5"
+            id="submit"
+            >Send <svg
+              class="w-5 h-5 ml-2 -mr-1"
+              fill="currentColor"
+              viewBox="0 0 20 20"
+              xmlns="http://www.w3.org/2000/svg"
+              ><path
+                fill-rule="evenodd"
+                d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
+                clip-rule="evenodd"
+              /></svg
+            ></button
+          >
+        </form>
       </div>
     </div>
-  </section>
+    <div class="grid gap-16 grid-cols-2" />
+  </div>
+</section>
