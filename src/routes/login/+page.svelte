6e1bf848adee75f16bb1641a54c8a85d839a9643
Avelyn Choi <avelynhc@gmail.com>
2023-10-31 16:35:18 -0400

    updated css and add a new method to clear user input when error occurs

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -1,5 +1,9 @@
 <script>
   let errorMsg = "";
+  function removeUserInput() {
+    document.getElementById("userMail").value = "";
+    document.getElementById("password").value = "";
+  };
   async function handleSubmit(event) {
     event.preventDefault();
 
@@ -24,19 +28,21 @@
         window.location.href = "/";
       } else {
         errorMsg = json.error;
+        removeUserInput();
       }
     } catch (error) {
       errorMsg = error;
+      removeUserInput();
     }
   }
 </script>
 
 <body
-  class="sm:w-[50%] w-full h-[50vh] m-auto border-2 border-gray-600 dark:focus:border-gray-500 rounded-xl flex items-center justify-center flex-col"
+  class="sm:w-[30%] w-full h-[50vh] m-auto border-2 border-gray-600 dark:focus:border-gray-500 rounded-xl flex items-center justify-center flex-col"
 >
   <h1 class="text-3xl font-extrabold text-gray-800 dark:text-white m-auto">
     {#if errorMsg}
-      <span class="text-red-600">
+      <span class="text-red-600 error">
         {errorMsg}
       </span>
     {:else}
@@ -48,9 +54,9 @@
     method="POST"
     class=" flex items-center justify-center flex-col space-y-4 m-auto"
   >
-    <div class=" space-y-2">
+    <div class="space-y-2">
       <label for="userMail" class="text-gray-600 dark:text-gray-400 text-lg"
-        >email:</label
+        >Email:</label
       >
       <input
         type="email"
@@ -63,7 +69,7 @@
     </div>
     <div class=" space-y-2">
       <label for="password" class="text-gray-600 dark:text-gray-400 text-lg"
-        >password:</label
+        >Password:</label
       >
       <input
         type="password"
@@ -76,7 +82,7 @@
     <button
       type="submit"
       class="py-2 px-8 inline-flex items-center justify-center text-base font-medium text-center text-white rounded-md bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-900"
-      >login
+      >Login
       <svg
         class="w-5 h-5 ml-2 -mr-1"
         fill="currentColor"
@@ -90,7 +96,7 @@
       >
     </button>
     <p>
-      New user? <a href="/signin">
+      <span class="white-font">New user? </span><a href="/signin">
         <span class="text-blue-500">Register here</span></a
       >
     </p>
